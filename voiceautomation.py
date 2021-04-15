a8af1b582bfd4b9ab2c729ad8babc9c1e68ed5d6
Nishantsingh-10 <62044520+Nishantsingh-10@users.noreply.github.com>
2021-04-15 12:01:38 +0530

    deleted

--- voiceautomation.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-record = sr.Recognizer()
-with sr.Microphone() as source:
-    print("what you want....")
-    audio=record.listen(source)
-    print("done.")
-data = record.recognize_google(audio)
-print(data)
